54a2bd164886a83980833e0c3b05c3d23d1cdb8f
David Friedrich <thefeddy@gmail.com>
2021-07-03 12:25:11 -0400

    removed some info from DTOs, hopefully this fixes it

--- src/movies/movies.dto.ts
+++ src/movies/movies.dto.ts
@@ -2,25 +2,16 @@ export class MoviesDTO {
     readonly id: number;
     readonly movie_id: string;
     readonly watched: number;
-    readonly watched_on: Date;
-    readonly added: Date;
-    readonly watched_with: string;
 }
 
 export class CreateMovieDTO {
     readonly id: number;
     readonly movie_id: string;
     readonly watched: number;
-    readonly watched_on: Date;
-    readonly added: Date;
-    readonly watched_with: string;
 }
 
 export class WatchedMovieDTO {
     readonly id: number;
     readonly movie_id: string;
     readonly watched: number;
-    readonly watched_on: Date;
-    readonly added: Date;
-    readonly watched_with: string;
 }
